08b86737b75072352fbda58f36f0ab37fd3c5b73
anamella <amellaheanas@gmail.com>
2025-06-15 20:17:25 +0100

    3. Fetching Data from an API and Displaying It

---
+++ fetch-data.js
@@ -0,0 +1,31 @@
+
+
+async function fetchData()
+{
+    const dataContainer =  document.getElementById("api-data");
+    const apiUrl =  'https://jsonplaceholder.typicode.com/users';
+    try
+    {
+        const response =  await fetch(apiUrl);
+        if (!response.ok)
+            throw new Error("Error could not fetch data");
+        const users = await response.json();
+        const userList = document.createElement("ul");
+        users.forEach(element => {
+            const li =  document.createElement("li");
+            li.textContent = element.name;
+            userList.appendChild(li);
+        });
+        dataContainer.innerHTML = "";
+        dataContainer.appendChild(userList);
+    }
+    catch(error)
+    {
+        console.error(error);
+        dataContainer.textContent = "Failed to load user data.";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+})
